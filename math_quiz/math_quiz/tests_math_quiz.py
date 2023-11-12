882a664fd286e2fb102b2ae58e16c664f731e0b4
Harinder Singh Bhachu <harinderbhachu@gmail.com>
2023-11-13 00:29:05 +0100

    Test case scenareo written for the three functions

--- math_quiz/math_quiz/tests_math_quiz.py
+++ math_quiz/math_quiz/tests_math_quiz.py
@@ -1,29 +1,43 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_number, generate_operator, perform_operation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_number(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_number(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
+    def test_generate_operator(self):
+        # Run the function to get an operator
+        operator = generate_operator()
+        
+        # Define the valid operators
+        valid_operators = {'+', '-', '*'}
+        
+        # Check if the generated operator is in the valid_operators set
+        self.assertIn(operator, valid_operators, f"Generated operator '{operator}' is not valid.")
         pass
 
-    def test_function_C(self):
+    def test_perform_operation(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+            (5, 2, '+', '5 + 2', 7),
+            (8, 3, '-', '8 - 3', 5),
+            (4, 6, '*', '4 * 6', 24)
+            # Add more test cases as needed
+        ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
+                # Run the function
+                equation, result = perform_operation(num1, num2, operator)
+
+                # Check the output
+                self.assertEqual((equation, result), (expected_problem, expected_answer), f"Test failed for {num1} {operator} {num2}")
+
                 pass
 
 if __name__ == "__main__":
